c9feb42cff5808e35fdbe1c1105915befa04b9a6
Saifullah Khan <143829717+saif72437@users.noreply.github.com>
2025-10-16 10:36:01 -0700

    WEEK 1.3 | loops

---
+++ WEEK 01/WEEK 1.3/loop2.js
@@ -0,0 +1,11 @@
+// find sum from 1 to 10
+// let ans = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+// console.log(ans);
+
+let sum = 0;
+
+for(let i = 1; i <= 5; i++){
+    sum = sum + i
+}
+
+console.log(sum);
---
+++ WEEK 01/WEEK 1.3/loop4.js
@@ -0,0 +1,9 @@
+let i = 1;
+
+do{
+    console.log(i);
+    i++
+    
+}while(i >= 5)
+
+    // it will run one time if the condition is false
